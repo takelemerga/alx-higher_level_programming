c7f4eab2c64d4bc5b85071fec7417d0f69c5ecee
tolaa <mgamtaa@gmail.com>
2021-11-18 09:00:07 +0300

    directory name changed

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for m in range(0, 10):
+    for n in range(m + 1, 10):
+        if m == 8 and n == 9:
+            print("{}{}".format(m, n))
+        else:
+            print("{}{}".format(m, n), end=", ")
