a03d3ede1247ba7ec2ec5464c23fa079b4d653a1
tolaa <mgamtaa@gmail.com>
2021-11-23 09:38:49 +0300

    added a program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-# 3-infinite_add.py
-
 
 if __name__ == "__main__":
-    """Print sum of args."""
-    import sys
+    import hidden_4
 
-    sum = 0
-    for i in range(len(sys.argv) - 1):
-        sum += int(sys.argv[i + 1])
-    print("{}".format(sum))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
