5d641fd052c6d3f6788f6cae224a953e38165fb1
tolaa <mgamtaa@gmail.com>
2021-12-06 14:18:09 +0300

    added a function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,15 @@
+ 101-safe_function.py
+
+
+import sys
+
+
+def safe_function(fct, *args):
+    
+
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
