a1f6c6fd625f4a8ca653d60d0833dad0ecab14dc
tolaa <mgamtaa@gmail.com>
2021-12-06 12:27:05 +0300

    added a function that divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+
+    try:
+        quotient = a / b
+    except (TypeError, ZeroDivisionError):
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+    return (quotient)
