0ddbfe48dabe95ba2964f881ba1f62b34019879b
tolaa <mgamtaa@gmail.com>
2021-12-08 09:57:13 +0300

    a class Square that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+"""define class square"""
 
 
 class Square:
 
-    """define class square"""
+    """constractor"""
 
     def __init__(self, size=0):
-
-        """constractor"""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
