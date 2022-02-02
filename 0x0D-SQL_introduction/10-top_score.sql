fdeda4c542cb7fb847e473d7abcc2bb51e99a11c
tolaa <mgamtaa@gmail.com>
2022-02-02 11:18:19 +0300

    basic SQL command

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- list name and score columns from second_table by ordering score in descending order
-SELECT name, score FROM second_table
+SELECT score, name FROM second_table
 ORDER BY score DESC;
 
