3cd3b3d8da6f51a819c8b0324328aa34eb6b6753
tolaa <mgamtaa@gmail.com>
2022-02-28 12:30:25 +0300

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const args = require('process');
+console.log(args.argv[2] + ' is ' + args.argv[3]);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = require('process');
+const arg2 = parseInt(args.argv[2]);
+if (isNaN(arg2)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + arg2);
+}
