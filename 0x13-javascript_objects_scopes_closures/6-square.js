1563f56a00bc590689839873c2737b53e66ed266
tolaa <mgamtaa@gmail.com>
2022-03-01 11:33:10 +0300

    defines javascript class and deal with objects

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const square = require('./5-square');
+
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let k = 0; k < this.height; k++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
