800d5642fe437a53f75ee54fe1e941813481e998
tolaa <mgamtaa@gmail.com>
2022-03-05 12:38:25 +0300

    added python-object_relational_mapping code

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+"""
+contains class definition of a state and an instance
+Base = declarative_base()
+"""
+Base = declarative_base()
+
+
+class State(Base):
+    """ 
+    maps to table states
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
