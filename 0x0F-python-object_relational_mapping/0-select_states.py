ef244b594ec6744989ab51c15804faa13c8e1a9b
tolaa <mgamtaa@gmail.com>
2022-03-07 09:11:53 +0300

    added python-object_relational_mapping code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 from sys import argv
 
-"""a script that lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
