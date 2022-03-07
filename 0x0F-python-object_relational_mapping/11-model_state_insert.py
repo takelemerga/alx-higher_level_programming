8d176dfd4b08fc7baff0a723d52a56c9617b1d87
tolaa <mgamtaa@gmail.com>
2022-03-07 09:49:25 +0300

    added python-object_relational_mapping code

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+"""
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
-"""
-adds the State object “Louisiana” to the database hbtn_0e_6_usa
-"""
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(argv[1], argv[2], argv[3]),
