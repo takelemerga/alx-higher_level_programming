b407a30ab4c7cbcdc70a45ba38a5e6866389ae4e
tolaa <mgamtaa@gmail.com>
2022-03-18 11:38:28 +0300

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
