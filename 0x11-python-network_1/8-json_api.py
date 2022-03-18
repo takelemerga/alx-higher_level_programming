b2c42e53636dba85b8c1696f679e976c5bd217c9
tolaa <mgamtaa@gmail.com>
2022-03-18 11:43:14 +0300

    send letter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
+    try:
+        data = result.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
