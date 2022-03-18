23783b723a466649ac4a9e7e1f95679f50be9155
tolaa <mgamtaa@gmail.com>
2022-03-18 11:40:58 +0300

    send post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+That takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally
+displays the body of the response.
+"""
+
+import requests
+import sys
+
+
+def postemail():
+    """sends post"""
+    email = {'email': sys.argv[2]}
+    response = requests.post(sys.argv[1], email)
+    print('{}'.format(response.text))
+
+
+if __name__ == "__main__"":
+    postemail()
