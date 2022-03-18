0381c8bb1a3abe637e3b2c2521173b612a070207
tolaa <mgamtaa@gmail.com>
2022-03-18 11:42:22 +0300

    display body response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the
+body of the response using requests
+"""
+import requests
+from sys import argv
+
+
+def error():
+    """
+    takes in a URL, sends a request to the URL and displays the
+    body of the response using requests
+    """
+    url = argv[1]
+    result = requests.get(url)
+    if result.status_code >= 400:
+        print("Error code: {}".format(result.status_code))
+    else:
+        print(result.text)
+
+
+if __name__ == "__main__":
+    error()
