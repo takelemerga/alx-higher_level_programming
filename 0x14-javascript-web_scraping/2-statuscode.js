f3cf186c0e5b4a633a44c0572b25f97625fcdd55
tolaa <mgamtaa@gmail.com>
2022-04-13 10:39:25 +0300

    fix semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // A script that display the status code of a GET request
 
 const url = process.argv[2];
-let request = require('request');
+const request = require('request');
 request(url, (error, response, body) => {
   if (error) {
     console.log('error:', error);
