681397160652f1c76008b2856c8f955864ad5531
tolaa <mgamtaa@gmail.com>
2022-06-16 14:23:02 +0300

    add, remove and clear script added

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,15 @@
+// adds, removes and clears LI elements from a list when the user clicks
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
