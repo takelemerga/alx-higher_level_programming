bdee129ba81892ff3ba272a91060a53d60f7a7ff
tolaa <mgamtaa@gmail.com>
2022-09-11 13:58:33 +0300

    list all state script added

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,6 +3,7 @@
 lists all State objects from the database hbtn_0e_6_usa
 """
 from sys import argv
+
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
