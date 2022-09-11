7ba2d12660bc91cccc182ff9fcae239649617501
tolaa <mgamtaa@gmail.com>
2022-09-11 14:29:54 +0300

    delete object from database

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -4,6 +4,7 @@ deletes all State objects with a name containing the letter a
 from the database hbtn_0e_6_usa
 """
 from sys import argv
+
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
