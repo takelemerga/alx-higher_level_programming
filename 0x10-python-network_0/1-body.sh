5c2515286dbe01dd802a451eaa6a94049587dfb9
tolaa <mgamtaa@gmail.com>
2022-09-22 11:29:16 +0300

    200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends GET request to that URL and displays size of body
+# takes in a URL, sends GET request to that URL and displays only body of a 200 status code response
 curl -sL "$1"
