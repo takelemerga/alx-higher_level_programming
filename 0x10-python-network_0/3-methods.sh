516fbc2e3924a46c0e4798e2d9718c9fb524bcd8
tolaa <mgamtaa@gmail.com>
2022-09-22 11:31:14 +0300

    displays all HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
+# takes in a URL and displays all HTTP methods that  will accepted by the server
 curl -sI "$1" | grep "Allow" | cut -d " " -f2-
