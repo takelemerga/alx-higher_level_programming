6af175170ca10ec4ef790bf3b2f43f81cffe1afd
tolaa <mgamtaa@gmail.com>
2022-09-22 12:22:50 +0300

    sends a GET request to the URL

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as argument, sends a GET request to URL and displays body of response and A header variable X-School-User-Id must be sent with the value 98
+# sends a GET request to URL and displays body of response
 curl -sH "X-School-User-Id: 98" "$1"
