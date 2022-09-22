e5c4eb8905fa23d6688d584ec37c3443221964f9
tolaa <mgamtaa@gmail.com>
2022-09-22 11:32:27 +0300

    set header variable value

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# sends a GET request to URL and displays body of response
+# takes in a URL as argument, sends a GET request to URL and displays body of response
+# A header variable X-School-User-Id must be sent with the value 98
 curl -sH "X-School-User-Id: 98" "$1"
