4b37a21df6bfabf40df15e9746b88cb753313269
tolaa <mgamtaa@gmail.com>
2022-09-22 11:36:09 +0300

    sends a JSON POST request to a URL

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Script sends a JSON POST request to a URL first argument, displays the body of response
+# the script sends a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request
 curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"
