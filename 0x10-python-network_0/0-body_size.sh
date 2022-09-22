4f239b29fb4dd8d1ba34c8826d3cc68b04395e80
tolaa <mgamtaa@gmail.com>
2022-09-22 11:28:00 +0300

    display response body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL and displays the size of the body of the response
+# Script that takes in a URL, sends a request to that URL and displays the size of the body of the response in bytes
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
