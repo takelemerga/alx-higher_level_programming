2c753e06a2fb1bc4111882fecc06620a449e5c2c
tolaa <mgamtaa@gmail.com>
2022-09-23 10:17:52 +0300

    display header variable value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the value of the
-variable X-Request-Id in the response header
+script takes in a URL, sends a request to the URL and displays the value
 """
 
 import requests
