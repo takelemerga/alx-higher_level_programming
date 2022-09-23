4d37c76430166efca83f47329a45a74bf25319d4
tolaa <mgamtaa@gmail.com>
2022-09-23 10:20:35 +0300

    check status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the
+script that takes in a URL, sends a request to the URL and displays the
 body of the response using requests
 """
 import requests
