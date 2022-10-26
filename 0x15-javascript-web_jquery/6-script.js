c1d5eb1a014e9e985bce8e7bff2a04a88131b3f4
tolaa <mgamtaa@gmail.com>
2022-10-26 09:35:26 +0300

    updates text of header

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,4 +1,4 @@
-// updates the text of the <header> element
+// updates z text of the <header> element
 // to New Header!!! when the user clicks on DIV#update_header
 $('div#update_header').click(function () {
   $('header').text('New Header!!!');
