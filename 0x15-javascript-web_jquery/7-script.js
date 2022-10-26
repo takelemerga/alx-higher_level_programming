8c8440912d9206449bf1e5f11159927d262b60df
tolaa <mgamtaa@gmail.com>
2022-10-26 09:37:31 +0300

    fetch character from url

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-// fetches the character 'name' from URL
+// fetches the character 'name' from given URL
 
 $.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
   $('DIV#character').html(data.name);
