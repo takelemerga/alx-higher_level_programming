58939785447b9d68728002ab6e9f08180c48a8e0
tolaa <mgamtaa@gmail.com>
2022-10-26 09:25:53 +0300

    class attribute added

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,4 +1,4 @@
-// adds the class red to the <header> element
+// adds the att class red to the <header> element
 // when the user clicks on the tag DIV#red_header
 
 $('DIV#red_header').click(function () {
